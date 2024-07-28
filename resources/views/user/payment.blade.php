9e6a9a10b5fc76b753817f916bddae1620df3fe2
bengsky13 <bengskysec13@gmail.com>
2024-07-28 22:10:29 +0700

    trx

--- resources/views/user/payment.blade.php
+++ resources/views/user/payment.blade.php
@@ -11,15 +11,13 @@
     <div class="container p-5">
       <div class="row justify-content-center">
         <div class="col-lg-10 border-light border-dotted" id="divFile">
-          <div class="container p-5" id="qr">
-           <h1 id="fileName"></h1>
+           <h1 id="fileName"> WAITING PAYMENT</h1>
           </div>
         </div>
       </div>
   </div>
   <script src="https://cdn.jsdelivr.net/npm/qrcodejs@1.0.0/qrcode.min.js"></script>
 <script>
-            new QRCode(document.getElementById("qr"), "{{$qr}}");
 
 </script>
   </body>
