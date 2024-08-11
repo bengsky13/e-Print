b0becbf5d6dc0a262ed679ff8c5546296741e03f
bengsky13 <bengskysec13@gmail.com>
2024-08-12 00:22:39 +0700

    update

--- routes/api.php
+++ routes/api.php
@@ -4,17 +4,6 @@ use App\Http\Controllers\ApiController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
-
-Route::get('/initialize', [ApiController::class, 'initialize']);
-Route::get('/{id}/status', [ApiController::class, 'status']);
-Route::post('/{id}/update', [ApiController::class, 'updateStatus']);
+Route::get('/initialize', [ApiController::class, 'initialize'])->withoutMiddleware('throttle');
+Route::get('/{id}/status', [ApiController::class, 'status'])->withoutMiddleware('throttle');
+Route::post('/{id}/update', [ApiController::class, 'updateStatus'])->withoutMiddleware('throttle');
